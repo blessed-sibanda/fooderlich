81e0141c8ea6f43e1724993da725a46122799cbf
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-10-23 20:44:43 +0200

    attempt author card

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fooderlich/card1.dart';
+import 'package:fooderlich/card2.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -13,9 +14,7 @@ class _HomeState extends State<Home> {
 
   static List<Widget> pages = <Widget>[
     Card1(),
-    Container(
-      color: Colors.green,
-    ),
+    Card2(),
     Container(
       color: Colors.blue,
     ),
