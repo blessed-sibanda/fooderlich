065a284c4334c0cbab7397d681f4fe114f540c63
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-10-23 21:07:08 +0200

    finish author card

--- lib/card2.dart
+++ lib/card2.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/author_card.dart';
+import 'package:fooderlich/fooderlich_theme.dart';
 
 class Card2 extends StatelessWidget {
   const Card2({Key? key}) : super(key: key);
@@ -14,8 +16,28 @@ class Card2 extends StatelessWidget {
             borderRadius: BorderRadius.all(Radius.circular(10.0))),
         child: Column(
           children: [
-            // TODO 1: add author information
-            // TODO 4: add Positioned text
+            const AuthorCard(
+                authorName: 'Mike Katz',
+                title: 'Smoothie Connoisseur',
+                imageProvider: AssetImage('assets/author_katz.jpeg')),
+            Expanded(
+                child: Stack(
+              children: [
+                Positioned(
+                    bottom: 16,
+                    right: 16,
+                    child: Text('Recipe',
+                        style: FooderlichTheme.lightTextTheme.headline1)),
+                Positioned(
+                    bottom: 70,
+                    left: 16,
+                    child: RotatedBox(
+                      quarterTurns: 3,
+                      child: Text('Smoothies',
+                          style: FooderlichTheme.lightTextTheme.headline1),
+                    )),
+              ],
+            ))
           ],
         ),
       ),
