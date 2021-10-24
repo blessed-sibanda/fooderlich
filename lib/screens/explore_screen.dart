646a75ef011f2db82f4f28bea16bd42a5a25fe87
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-10-24 09:50:31 +0200

    impement explore screen 'recipes of the day' listview

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fooderlich/api/mock_fooderlich_service.dart';
+import 'package:fooderlich/models/explore_data.dart';
 import '../components/components.dart';
-import '../fooderlich_theme.dart';
 
 class ExploreScreen extends StatelessWidget {
   final mockService = MockFooderlichService();
@@ -10,8 +10,17 @@ class ExploreScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Explore Screen'),
-    );
+    return FutureBuilder(
+        future: mockService.getExploreData(),
+        builder: (BuildContext context, AsyncSnapshot<ExploreData> snapshot) {
+          if (snapshot.hasData) {
+            final recipes = snapshot.data!.todayRecipes;
+            return TodayRecipeListView(recipes: recipes);
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
   }
 }
