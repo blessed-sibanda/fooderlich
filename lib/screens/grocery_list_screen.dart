c1a60a74fd903c304b2f3b5c63ee543b0a47bc60
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-10-31 09:19:23 +0200

    update grocery items

--- lib/screens/grocery_list_screen.dart
+++ lib/screens/grocery_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fooderlich/components/grocery_tile.dart';
 import 'package:fooderlich/models/grocery_manager.dart';
+import 'package:fooderlich/screens/grocery_item_screen.dart';
 
 class GroceryListScreen extends StatelessWidget {
   final GroceryManager manager;
@@ -16,12 +17,28 @@ class GroceryListScreen extends StatelessWidget {
           itemBuilder: (context, index) {
             final item = groceryItems[index];
             // TODO: Wrap in a Dismissable
-            // TODO: Wrap in a InkWell
-            return GroceryTile(
-                item: item,
-                onComplete: (change) {
-                  manager.completeItem(index, change!);
-                });
+
+            return InkWell(
+              child: GroceryTile(
+                  key: Key(item.id!),
+                  item: item,
+                  onComplete: (change) {
+                    manager.completeItem(index, change!);
+                  }),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => GroceryItemScreen(
+                              originalItem: item,
+                              onUpdate: (item) {
+                                manager.updateItem(item, index);
+                                Navigator.pop(context);
+                              },
+                              onCreate: (_) {},
+                            )));
+              },
+            );
           },
           separatorBuilder: (context, index) {
             return const SizedBox(height: 16.0);
